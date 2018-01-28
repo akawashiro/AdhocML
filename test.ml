d890a8a7cb54b1294a1c515971abd48b8f439a35
akira <a.kawashiro@gmail.com>
2018-01-28 15:57:01 +0900

    Pass all tests.

--- test.ml
+++ test.ml
@@ -1,5 +1,7 @@
-let rec f = fun x -> if 10 < x then 1 else  x * f (x + 1) in f 1;;
 let f = fun x -> x in f;;
+fun x -> x;;
+let f = fun x -> x in if f True then f 10 else 1;;
+let rec f = fun x -> if 10 < x then 1 else  x * f (x + 1) in f 1;;
 let f = 10 in f;;
 2 * 2;;
 19;;
