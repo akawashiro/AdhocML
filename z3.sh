7e8bf185b4e8be6a1a2956bd496cfc366b4c0917
Akira Kawata <akirakawata1@gmail.com>
2019-12-28 21:39:53 +0900

    Writing blog.

--- z3.sh
+++ z3.sh
@@ -4,4 +4,4 @@ stack build && cat z3.ml | stack exec hMLInterpreter-Z3-exe > z3tmp.py
 cat z3tmp.py
 echo "# ------------Result of Z3----------------"
 echo "# ty0 is the type of return value."
-python z3tmp.py
+python3 z3tmp.py
